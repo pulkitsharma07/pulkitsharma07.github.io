5fea90f74600ed20fe638634c619180743f17f85
pulkitsharma07 <avastpulkit@gmail.com>
2023-05-07 18:47:39 +0100

    Fix permissions

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker build -t nginx . -f Dockerfile.rpi
-docker run -d --restart=unless-stopped -p 9999:8080 -v /home/pulkit/Workspace/pulkitsharma07.github.io:/app nginx
+docker run -d --restart=unless-stopped -p 9999:8080 -v /home/pulkit/workspace/pulkitsharma07.github.io:/app nginx
